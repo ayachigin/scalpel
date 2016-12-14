b618b652a5e6c95e7deda0a00832f3c4eb03ecd8
Ayachi Gin <ayachigin@gmail.com>
2016-12-14 10:54:45 +0900

    Use wreq instead of curl

--- src/Text/HTML/Scalpel/Internal/Scrape/URL.hs
+++ src/Text/HTML/Scalpel/Internal/Scrape/URL.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_HADDOCK hide #-}
 module Text.HTML.Scalpel.Internal.Scrape.URL (
     URL
@@ -20,33 +21,36 @@ import Data.Char (toLower)
 import Data.Default (def)
 import Data.List (isInfixOf)
 import Data.Maybe (listToMaybe)
+import Data.ByteString.Char8 (unpack)
+import Control.Lens ((^.), (^?))
 
-import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BSL
 import qualified Data.Default as Default
-import qualified Data.Text.Encoding as Text
-import qualified Network.Curl as Curl
+import qualified Data.Text.Lazy.Encoding as Text
+import qualified Network.Wreq as Wreq
 import qualified Text.HTML.TagSoup as TagSoup
 import qualified Text.StringLike as TagSoup
 
 
+
 type URL = String
 
-type CurlResponse = Curl.CurlResponse_ [(String, String)] BS.ByteString
+type WreqResponse = Wreq.Response BSL.ByteString
 
 -- | A method that takes a HTTP response as raw bytes and returns the body as a
 -- string type.
-type Decoder str = Curl.CurlResponse_ [(String, String)] BS.ByteString -> str
+type Decoder str = Wreq.Response BSL.ByteString -> str
 
 -- | A record type that determines how 'scrapeUrlWithConfig' interacts with the
 -- HTTP server and interprets the results.
 data Config str = Config {
-    curlOpts :: [Curl.CurlOption]
+    wreqOpts :: Wreq.Options
 ,   decoder  :: Decoder str
 }
 
 instance TagSoup.StringLike str => Default.Default (Config str) where
     def = Config {
-            curlOpts = [Curl.CurlFollowLocation True]
+            wreqOpts = Wreq.defaults
         ,   decoder  = defaultDecoder
         }
 
@@ -54,13 +58,13 @@ instance TagSoup.StringLike str => Default.Default (Config str) where
 -- executes a 'Scraper' on it.
 scrapeURL :: (Ord str, TagSoup.StringLike str)
           => URL -> Scraper str a -> IO (Maybe a)
-scrapeURL = scrapeURLWithOpts [Curl.CurlFollowLocation True]
+scrapeURL = scrapeURLWithOpts Wreq.defaults
 
 -- | The 'scrapeURLWithOpts' function take a list of curl options and downloads
 -- the contents of the given URL and executes a 'Scraper' on it.
 scrapeURLWithOpts :: (Ord str, TagSoup.StringLike str)
-                  => [Curl.CurlOption] -> URL -> Scraper str a -> IO (Maybe a)
-scrapeURLWithOpts options = scrapeURLWithConfig (def {curlOpts = options})
+                  => Wreq.Options -> URL -> Scraper str a -> IO (Maybe a)
+scrapeURLWithOpts options = scrapeURLWithConfig (def {wreqOpts = options})
 
 -- | The 'scrapeURLWithConfig' function takes a 'Config' record type and
 -- downloads the contents of the given URL and executes a 'Scraper' on it.
@@ -71,20 +75,21 @@ scrapeURLWithConfig config url scraper = do
     return (maybeTags >>= scrape scraper)
     where
         downloadAsTags decoder url = do
-            maybeBytes <- openURIWithOpts url (curlOpts config)
+            maybeBytes <- openURIWithOpts url (wreqOpts config)
             return $ TagSoup.parseTags . decoder <$> maybeBytes
 
-openURIWithOpts :: URL -> [Curl.CurlOption] -> IO (Maybe CurlResponse)
+openURIWithOpts :: URL -> Wreq.Options -> IO (Maybe WreqResponse)
 openURIWithOpts url opts = do
-    resp <- curlGetResponse_ url opts
-    return $ if Curl.respCurlCode resp /= Curl.CurlOK
+    resp <- wreqGetResponse_ url opts
+    return $ if status resp /= 200
         then Nothing
         else Just resp
+      where status r = r ^. Wreq.responseStatus . Wreq.statusCode
 
-curlGetResponse_ :: URL
-                 -> [Curl.CurlOption]
-                 -> IO (Curl.CurlResponse_ [(String, String)] BS.ByteString)
-curlGetResponse_ = Curl.curlGetResponse_
+wreqGetResponse_ :: URL
+                 -> Wreq.Options
+                 -> IO WreqResponse
+wreqGetResponse_ = flip Wreq.getWith
 
 -- | The default response decoder. This decoder attempts to infer the character
 -- set of the HTTP response body from the `Content-Type` header. If this header
@@ -93,15 +98,10 @@ defaultDecoder :: TagSoup.StringLike str => Decoder str
 defaultDecoder response = TagSoup.castString
                         $ choosenDecoder body
     where
-        body        = Curl.respBody response
-        headers     = Curl.respHeaders response
-        contentType = listToMaybe
-                    $ map (map toLower . snd)
-                    $ take 1
-                    $ dropWhile ((/= "content-type") . map toLower . fst)
-                                headers
+        body        = response ^. Wreq.responseBody
+        contentType = response ^? Wreq.responseHeader "Content-Type"
 
-        isType t | Just ct <- contentType = ("charset=" ++ t) `isInfixOf` ct
+        isType t | Just ct <- contentType = ("charset=" ++ t) `isInfixOf` (unpack ct)
                  | otherwise              = False
 
         choosenDecoder | isType "utf-8" = Text.decodeUtf8
@@ -109,8 +109,10 @@ defaultDecoder response = TagSoup.castString
 
 -- | A decoder that will always decode using `UTF-8`.
 utf8Decoder ::  TagSoup.StringLike str => Decoder str
-utf8Decoder = TagSoup.castString . Text.decodeUtf8 . Curl.respBody
+utf8Decoder = TagSoup.castString . Text.decodeUtf8 .
+              (^. Wreq.responseBody)
 
 -- | A decoder that will always decode using `ISO-8859-1`.
 iso88591Decoder ::  TagSoup.StringLike str => Decoder str
-iso88591Decoder = TagSoup.castString . Text.decodeLatin1 . Curl.respBody
+iso88591Decoder = TagSoup.castString . Text.decodeLatin1 .
+                  (^. Wreq.responseBody)
